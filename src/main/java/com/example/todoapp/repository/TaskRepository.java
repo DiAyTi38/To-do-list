57044fd195322b7c83e7f1acde074b6cb647ef86
Trần Tiến Đạt <trandat@MacBook-Pro-cua-Tran.local>
2026-05-21 08:05:25 +0700

    Gộp nhánh category vào dự án

--- src/main/java/com/example/todoapp/repository/TaskRepository.java
+++ src/main/java/com/example/todoapp/repository/TaskRepository.java
@@ -4,6 +4,8 @@ import com.example.todoapp.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByCategory(String category);
